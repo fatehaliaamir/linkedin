ad1a0373a843744eadf245ccbe158785305b41d0
Fateh Ali Aamir <fateh.ali@dubizzlelabs.com>
2023-11-07 18:59:33 +0500

    comments cruds finalised, can add and remove comment

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,26 +1,53 @@
 const Comment = require('../models/comment');
 
-exports.getComments = (req, res) => {
-    // Your logic to fetch and list comments (e.g., from a database)
-    const comments = ['Comment 1', 'Comment 2', 'Comment 3']; // Replace with actual comment data
-    res.json(comments);
-  };
-
-exports.getAddComment = (req, res) => {
-    // Your logic to render the comment form
-    res.send('Comment form'); // Replace with your actual form rendering logic
-  };
-
-exports.postAddComment = (req, res) => {
-    // Your logic to process the comment form data, e.g., save to a database
-    const newComment = req.body; // Assuming comment data is sent in the request body
-    // Process and save the comment data
-    res.send('Comment successfully added'); // Replace with your actual response
-  };
-
-exports.postDeleteComment = (req, res) => {
-    const commentId = req.body.commentId; // Assuming you send the comment ID in the request body
-    // Your logic to delete the comment, e.g., from a database
-    res.send(`Comment with ID ${commentId} deleted`); // Replace with your actual response
+exports.getComments = async (req, res) => {
+    try {
+      // Use Sequelize to fetch all comments from the 'Comment' model
+      const comments = await Comment.findAll();
+  
+      res.json(comments);
+    } catch (error) {
+      console.error('Error fetching comments:', error);
+      res.status(500).json({ error: 'An error occurred while fetching comments.' });
+    }
   };
   
+  exports.postAddComment = async (req, res) => {
+    const content = req.body.content;
+    const post_id = req.body.post_id;
+    const user_id = req.body.user_id; // Assuming comment data is sent in the request body
+  
+    try {
+      // Use Sequelize to create a new comment record in the 'Comment' model
+      const newComment = await Comment.create({
+        content,
+        post_id, // Assuming postId corresponds to the post the comment is related to
+        user_id, // Assuming userId corresponds to the user who made the comment
+      });
+  
+      res.send('Comment successfully added');
+    } catch (error) {
+      console.error('Error adding comment:', error);
+      res.status(500).json({ error: 'An error occurred while adding the comment.' });
+    }
+  };
+
+  exports.postDeleteComment = async (req, res) => {
+    const comment_id = req.body.comment_id; // Assuming you send the comment ID in the request body
+  
+    try {
+      // Use Sequelize to delete the comment from the 'Comment' model
+      const deletedComment = await Comment.destroy({
+        where: { comment_id: comment_id }, // Assuming the comment ID corresponds to the 'id' field in your Comment model
+      });
+  
+      if (deletedComment) {
+        res.send(`Comment with ID ${comment_id} deleted`);
+      } else {
+        res.status(404).json({ error: 'Comment not found' });
+      }
+    } catch (error) {
+      console.error('Error deleting comment:', error);
+      res.status(500).json({ error: 'An error occurred while deleting the comment.' });
+    }
+  };
--- routes/comment.js
+++ routes/comment.js
@@ -7,12 +7,12 @@ const commentController = require('../controllers/comment');
 const router = express.Router();
 
 //commenting crud
-router.get('/posts/comments', commentController.getComments);
+router.get('/comments', commentController.getComments);
 
-router.get('/posts/comments/add-comment', commentController.getAddComment);
+//router.get('/comments/add-comment', commentController.getAddComment);
 
-router.post('/posts/comments/add-comment', commentController.postAddComment);
+router.post('/comments/add-comment', commentController.postAddComment);
 
-router.post('/posts/comments/delete-comment', commentController.postDeleteComment);
+router.post('/comments/delete-comment', commentController.postDeleteComment);
 
 module.exports = router;
