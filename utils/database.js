02abde34aa040a00bb0ea7359a2efad9aa47d483
Fateh Ali Aamir <fateh.ali@dubizzlelabs.com>
2023-11-10 11:19:56 +0500

    basic app structure

--- utils/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('linkedin', 'root', 'Haider@123', {
-    dialect: 'mysql',
-    host: 'localhost'
-  });
-
-module.exports = sequelize;
